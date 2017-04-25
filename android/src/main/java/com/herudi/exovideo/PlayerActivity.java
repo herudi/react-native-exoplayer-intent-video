4cbfcf01fabd634f4a0a63d4beab8d20029eaf48
herudi7@gmail.com <herudi7@gmail.com>
2017-04-25 13:03:53 +0700

    fullscreen view

--- android/src/main/java/com/herudi/exovideo/PlayerActivity.java
+++ android/src/main/java/com/herudi/exovideo/PlayerActivity.java
@@ -123,6 +123,20 @@ public class PlayerActivity extends Activity {
 
     }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+
 
     @Override
     protected void onStop() {
